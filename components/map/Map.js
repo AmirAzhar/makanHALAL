b6a2d00ca84c0fec90a5f600214dae5a648badcc
Amir Azhar <amir97azhar@gmail.com>
2021-12-18 22:35:49 +0800

    Add infoWindow

--- components/map/Map.js
+++ components/map/Map.js
@@ -1,25 +1,34 @@
+// React
+import { useState } from "react";
+
 // Google Maps API
 import {
   GoogleMap,
   useLoadScript,
   Marker,
   InfoWindow,
+  MarkerClusterer,
 } from "@react-google-maps/api";
 
 // Google Maps Constants
 import mapConstants from "./mapConstants";
+import markers from "./fakeMapData";
 
 // Components
 import FullMenu from "../sidebar/FullMenu";
 
 function Map() {
+  const [selected, setSelected] = useState(null);
+  // Init the google map API
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_JAVASCRIPT_API,
     libraries: mapConstants.libraries,
   });
 
   if (loadError) return "Error loading maps";
-  if (!isLoaded) return "Loading maps...";
+  if (!isLoaded) {
+    return "Loading maps...";
+  }
 
   return (
     <div>
@@ -29,7 +38,36 @@ function Map() {
         zoom={mapConstants.zoom}
         center={mapConstants.center}
         options={mapConstants.options}
-      ></GoogleMap>
+      >
+        {markers.map((marker) => (
+          <Marker
+            key={marker.address}
+            position={{ lat: marker.lat, lng: marker.lng }}
+            icon={{
+              url: "/halalMarker.svg",
+              scaledSize: new window.google.maps.Size(50, 50),
+              origin: new window.google.maps.Point(0, 0),
+              anchor: new window.google.maps.Point(15, 15),
+            }}
+            onClick={() => {
+              setSelected(marker);
+            }}
+          />
+        ))}
+        {selected ? (
+          <InfoWindow
+            position={{ lat: selected.lat, lng: selected.lng }}
+            onCloseClick={() => {
+              setSelected(null);
+            }}
+          >
+            <div>
+              <h4>{selected.name}</h4>
+              <h4>{selected.address}</h4>
+            </div>
+          </InfoWindow>
+        ) : null}
+      </GoogleMap>
     </div>
   );
 }
---
+++ components/map/fakeMapData.js
@@ -0,0 +1,22 @@
+const markers = [
+  {
+    name: "Starbucks",
+    address: "1 Pasir Ris Central Street 3 01-21/22/23 White Sands, 518457",
+    lat: 1.3723935974018306,
+    lng: 103.9498648313885,
+  },
+  {
+    name: "Rasa Istimewa Restaurant",
+    address: "201 Pasir Ris Rd, Singapore 519147",
+    lat: 1.3821107865925586,
+    lng: 103.94783454881325,
+  },
+  {
+    name: "McDonald's",
+    address: "258 Pasir Ris Street 21, #01-25, Loyang Point, Singapore 510258",
+    lat: 1.3667177424441177,
+    lng: 103.96412293158309,
+  },
+];
+
+export default markers;
